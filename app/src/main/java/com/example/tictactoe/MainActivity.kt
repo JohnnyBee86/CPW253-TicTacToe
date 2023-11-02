62df14d639f295ec8460f33f3bed435418cc1253
JohnnyBee86 <BartholomewJohn86@gmail.com>
2023-11-02 11:29:32 -0700

    Added code to implement end game states and lock game grid when end game state is reached.  Added comments to explain variables and functions.

--- app/src/main/java/com/example/tictactoe/MainActivity.kt
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -1,41 +1,47 @@
 package com.example.tictactoe
 
-import android.content.Context
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var buttons: Array<Button>
-    private lateinit var messageBox: TextView
-    private var currentPlayerX = true
+    private lateinit var buttons: Array<Button> // contains the buttons that make up the play grid
+    private lateinit var messageBox: TextView   // the text view where messages will be displayed
+    private var currentPlayerX = true           // used to track if X is the current player
+    private var continuePlay = true             // used to track if the game is still playable
 
 
+    // on initial load:
+    //  assigns button values to the button array,
+    //  calls newGame() to start game,
+    //  and sets buttonNewGame onClickListener
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val button1 = findViewById<Button>(R.id.button)
-        val button2 = findViewById<Button>(R.id.button2)
-        val button3 = findViewById<Button>(R.id.button3)
-        val button4 = findViewById<Button>(R.id.button4)
-        val button5 = findViewById<Button>(R.id.button5)
-        val button6 = findViewById<Button>(R.id.button6)
-        val button7 = findViewById<Button>(R.id.button7)
-        val button8 = findViewById<Button>(R.id.button8)
-        val button9 = findViewById<Button>(R.id.button9)
+        val button1 = findViewById<Button>(R.id.button)     // top left grid button
+        val button2 = findViewById<Button>(R.id.button2)    // top middle grid button
+        val button3 = findViewById<Button>(R.id.button3)    // top right grid button
+        val button4 = findViewById<Button>(R.id.button4)    // middle left grid button
+        val button5 = findViewById<Button>(R.id.button5)    // center grid button
+        val button6 = findViewById<Button>(R.id.button6)    // middle right grid button
+        val button7 = findViewById<Button>(R.id.button7)    // bottom left grid button
+        val button8 = findViewById<Button>(R.id.button8)    // bottom middle grid button
+        val button9 = findViewById<Button>(R.id.button9)    // bottom right grid button
         messageBox = findViewById(R.id.textView)
         buttons = arrayOf(button1, button2, button3, button4, button5, button6, button7, button8, button9)
-        currentPlayerX = true
         newGame(buttons)
 
+        // assigns actions to the 'New Game' button
         val buttonNewGame = findViewById<Button>(R.id.button10)
         buttonNewGame.setOnClickListener{
             newGame(buttons)
+            continuePlay = true
         }
     }
 
+    // changes the message displayed based on the current turn
     private fun displayMessage(){
         if (currentPlayerX) {
             messageBox.text = getString(R.string.playerX)
@@ -45,6 +51,20 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // displays a message when play will no longer continue
+    private fun displayEndMessage(endState:String){
+        if (endState == "X") {
+            messageBox.text = getString(R.string.playerXWins)
+        }
+        else if (endState == "O") {
+            messageBox.text = getString(R.string.playerOWins)
+        }
+        else{
+            messageBox.text = getString(R.string.tieGame)
+        }
+    }
+
+    // resets the game
     private fun newGame(buttons : Array<Button> ) {
         for (button in buttons) {
             button.text = ""
@@ -54,11 +74,15 @@ class MainActivity : AppCompatActivity() {
         displayMessage()
     }
 
+    // executes when a player selects a button
     fun btnClick(view: View) {
-        val buttonSelected = view as Button
-        playGame(buttonSelected)
+        if (continuePlay) {
+            val buttonSelected = view as Button
+            playGame(buttonSelected)
+        }
     }
 
+    // marks a button based on the current player and checks if play will continue
     private fun playGame(buttonSelected: Button) {
         if (buttonSelected.text.equals("")) {
             if (currentPlayerX) {
@@ -69,7 +93,42 @@ class MainActivity : AppCompatActivity() {
                 buttonSelected.text = "O"
                 currentPlayerX = true
             }
-            displayMessage()
+            checkWinOrTie()
+            if (continuePlay) {
+                displayMessage()
+            }
+        }
+    }
+
+    // checks for end game conditions and if game is over sets continuePlay to false
+    private fun checkWinOrTie() {
+        if (buttons[0].text == "X" && buttons[1].text == "X" && buttons[2].text == "X" ||
+            buttons[3].text == "X" && buttons[4].text == "X" && buttons[5].text == "X" ||
+            buttons[6].text == "X" && buttons[7].text == "X" && buttons[8].text == "X" ||
+            buttons[0].text == "X" && buttons[3].text == "X" && buttons[6].text == "X" ||
+            buttons[1].text == "X" && buttons[4].text == "X" && buttons[7].text == "X" ||
+            buttons[2].text == "X" && buttons[5].text == "X" && buttons[8].text == "X" ||
+            buttons[0].text == "X" && buttons[4].text == "X" && buttons[8].text == "X" ||
+            buttons[2].text == "X" && buttons[4].text == "X" && buttons[6].text == "X") {
+                displayEndMessage("X")
+                continuePlay = false
+        }
+        else if (buttons[0].text == "O" && buttons[1].text == "O" && buttons[2].text == "O" ||
+            buttons[3].text == "O" && buttons[4].text == "O" && buttons[5].text == "O" ||
+            buttons[6].text == "O" && buttons[7].text == "O" && buttons[8].text == "O" ||
+            buttons[0].text == "O" && buttons[3].text == "O" && buttons[6].text == "O" ||
+            buttons[1].text == "O" && buttons[4].text == "O" && buttons[7].text == "O" ||
+            buttons[2].text == "O" && buttons[5].text == "O" && buttons[8].text == "O" ||
+            buttons[0].text == "O" && buttons[4].text == "O" && buttons[8].text == "O" ||
+            buttons[2].text == "O" && buttons[4].text == "O" && buttons[6].text == "O") {
+                displayEndMessage("O")
+                continuePlay = false
+        }
+        else if (buttons[0].text != "" && buttons[1].text != "" && buttons[2].text != "" &&
+            buttons[3].text != "" && buttons[4].text != "" && buttons[5].text != "" &&
+            buttons[6].text != "" && buttons[7].text != "" && buttons[8].text != "") {
+                displayEndMessage("Tie")
+                continuePlay = false
         }
     }
 }
